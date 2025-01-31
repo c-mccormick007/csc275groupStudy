878547ef94573f14aaf8c610fa9d16091c46ddd2
c-mccormick007 <c.mccormick1295@gmail.com>
2025-01-31 09:51:32 -0600

    notes added

--- csc275groupStudy/src/todoList/Task.java
+++ csc275groupStudy/src/todoList/Task.java
@@ -1,6 +1,8 @@
 package todoList;
 
 //todo: create a CLI task list that uses IO and exception handling.
+//classes & objects: inheritance and polymorphism
+//IO & exception handling: File, Scanner, BufferedReader, PrintWriter, BufferedWriter, FileWriter
 
 public class Task {
 
