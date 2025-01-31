e8e6a5cd457ac7aa9b6a54ec6423cff36268b9ea
c-mccormick007 <c.mccormick1295@gmail.com>
2025-01-30 18:06:32 -0600

    Initalize study group repo.

---
+++ csc275groupStudy/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module csc275groupStudy {
+}
---
+++ csc275groupStudy/src/todoList/Task.java
@@ -0,0 +1,7 @@
+package todoList;
+
+//todo: create a CLI task list that uses IO and exception handling.
+
+public class Task {
+
+}
